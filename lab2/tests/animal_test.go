61efa53ebe3ed315313ec8f8cd8e448c1125d2e3
Davlany <fedotzet6@gmail.com>
2024-06-16 14:17:22 +0300

    po ymy

--- lab2/tests/animal_test.go
+++ lab2/tests/animal_test.go
@@ -1,13 +1,24 @@
-package an_test
+package animal_test
 
-import "testing"
+import (
+	animalfactory "lab2/AnimalFactory"
+	"testing"
+)
 
 func AnimalTest(t *testing.T) {
 	testData := []string{"cat", "cat", "dog", "cat", "dog", "dog", "dog"}
-	expectedData := []string{"Meow","Meow","Gaw","Meow","Gaw","Gaw","Gaw"}
+	expectedData := []string{"Meow", "Meow", "Gaw", "Meow", "Gaw", "Gaw", "Gaw"}
 	for i, v := range testData {
-		if v != expectedData[i]{
-			t.Error("Bad test")
+		animal, err := animalfactory.AnimalFactory(v)
+		if err != nil {
+			s := animal.Say()
+			if s == expectedData[i] {
+				t.Log("Passed")
+			} else {
+				t.Errorf("Error, expected value %s, get %s", expectedData[i], v)
+			}
+		} else {
+			t.Errorf(err.Error())
 		}
 	}
 
